70bf6b11758386df3df48ebb131897a74cbb3f16
NelsonNNN <nelson.nyabuto51@gmail.com>
2020-06-24 00:23:47 +0300

    hide and display buttons

--- src/components/Additem.js
+++ src/components/Additem.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import PropTypes from 'prop-types'
 
 const firstState = {
@@ -8,14 +8,27 @@ const firstState = {
 }
 
 function Additem(props) {
+
+    const add = useRef()
+    const update = useRef()
+    const deleted = useRef()
    
     const [state, changeState] = useState(firstState)
 
     const onType = (e) =>{
         changeState({...state, [e.target.name]:e.target.value})
     }
+    const isFirstRun = useRef(false)
     useEffect(()=>{
         changeState({...state, name:props.data.name, calories:props.data.calories, id:props.data.id})
+        if(isFirstRun.current){
+            add.current.style.display = "none";
+            update.current.style.display = "inline";
+            deleted.current.style.display = "inline";
+        }else{
+            isFirstRun.current = true;
+        }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [props.data])
 
@@ -27,11 +40,17 @@ function Additem(props) {
     const onUpdate = ()=>{
         props.updateItem(state)
         changeState(firstState)
+        add.current.style.display = "inline";
+        update.current.style.display = "none";
+        deleted.current.style.display = "none";
     }
 
     const onDelete = ()=>{
         props.deleteItem(state.id)
         changeState(firstState)
+        add.current.style.display = "inline";
+        update.current.style.display = "none";
+        deleted.current.style.display = "none";
     }
     
 
@@ -45,9 +64,9 @@ function Additem(props) {
                 <input className={'calories-name'} type='number' name='calories' placeholder='Add Calories' value={state.calories} onChange={onType}/>
                 <label>Calories</label>
             </div>
-            <button onClick={onSubmit} className="add-btn btn blue darken-3"><i className="fa fa-plus">Add Meal</i></button>
-            <button onClick={onUpdate} className="update-btn btn orange"><i className="fa fa-pencil-square-o">Update Meal</i></button>
-            <button onClick={onDelete} className="delete-btn btn red"><i className="fa fa-remove">Delete Meal</i></button>
+            <button style={{display:'inline'}} ref={add} onClick={onSubmit} className="add-btn btn blue darken-3"><i className="fa fa-plus">Add Meal</i></button>
+            <button style={{display:'none'}}  ref={update} onClick={onUpdate} className="update-btn btn orange"><i className="fa fa-pencil-square-o">Update Meal</i></button>
+            <button style={{display:'none'}}  ref={deleted} onClick={onDelete} className="delete-btn btn red"><i className="fa fa-remove">Delete Meal</i></button>
         </div>
     )
 }
