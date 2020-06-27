41ffc27a35b9c1ec5427763132bde4b335a83a88
NelsonNNN <nelson.nyabuto51@gmail.com>
2020-06-27 23:07:15 +0300

    pagination button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -8,18 +8,35 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   }
 
   return (
-    <nav>
-      <ul>
+    <nav style={getStyle}>
+      <ul style={ulist}>
         {pageNumbers.map(number => (
           <li key={number} >
-            <a onClick={() => paginate(number)} href='!#' >
+            <button style={listStyle} onClick={() => paginate(number)} >
               {number}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
     </nav>
   );
 };
+const getStyle ={
+  backgroundColor:'transparent',
+  border:'none'
+}
+const ulist={
+  display:'flex',
+  flexWrap:'wrap'
+}
+const listStyle={
+  width:'3rem',
+  height:'3rem',
+  color:'white',
+  backgroundColor:'blue',
+  textAlign:'center',
+  boxShadow:'1px 1px 2px black',
+  marginRight:'3px'
+}
 
 export default Pagination;
